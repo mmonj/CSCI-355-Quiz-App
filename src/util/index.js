e0e180619fd004eddfc9abf6a9f695eae779f88f
mmonj <mmonj@users.noreply.github.com>
2025-03-30 13:54:50 -0400

    implement util functions

---
+++ src/util/index.js
@@ -0,0 +1,35 @@
+import { readFile } from "node:fs/promises";
+
+/** returns a random number between [min, max] inclusive
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+export function randNum(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
+ * @param {number} limit
+ * @returns {Promise<TQuestion[]>}
+ */
+export async function getQuestions(limit) {
+  const text = await readFile("src/data/questions.json", "utf-8");
+
+  /**@type {TQuestion[]} */
+  // @ts-ignore
+  const allQuestions = JSON.parse(text).map((question, idx) => {
+    return { ...question, id: idx };
+  });
+
+  const indexes = new Set();
+  while (indexes.size != limit) {
+    indexes.add(randNum(0, allQuestions.length - 1));
+  }
+
+  const questions = allQuestions.filter((_, idx) => indexes.has(idx));
+
+  return questions;
+}
