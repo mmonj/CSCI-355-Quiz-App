6335827989e6c7cb8cb48d5885608072650a7756
mmonj <mmonj@users.noreply.github.com>
2025-03-30 13:55:09 -0400

    implement `quiz` route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,15 @@
 /** @typedef {import('express').RequestHandler} RequestHandler */
 
+import { getQuestions } from "../util/index.js";
+
 /** @type {RequestHandler} */
 export const index = (req, res) => {
   res.render("index");
 };
 
-/** @type {RequestHandler} */
-export const quiz = (req, res) => {
-  res.send("Hello there");
+/** @type {AsyncRequestHandler} */
+export const quiz = async (req, res) => {
+  const questions = await getQuestions(10);
+
+  res.render("quiz", { questions });
 };
